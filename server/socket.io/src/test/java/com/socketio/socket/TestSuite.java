6002420898c5210ba4bafc7b4c512a137eb3f4c1
VitorManoelLP <vitor.m.lima.p@gmail.com>
2023-01-17 20:56:09 -0300

    fix/junit

---
+++ server/socket.io/src/test/java/com/socketio/socket/TestSuite.java
@@ -0,0 +1,12 @@
+package com.socketio.socket;
+
+import com.socketio.socket.resource.SocketResourceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        SocketResourceTest.class
+})
+class TestSuite {
+}
--- server/socket.io/src/test/java/com/socketio/socket/resource/SocketResourceTest.java
+++ server/socket.io/src/test/java/com/socketio/socket/resource/SocketResourceTest.java
@@ -1,9 +1,11 @@
 package com.socketio.socket.resource;
 
 import com.socketio.socket.service.MessagingService;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,6 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @RunWith(MockitoJUnitRunner.class)
 public class SocketResourceTest {
 
+    @InjectMocks
     private SocketResource resource;
 
     private MockMvc mockMvc;
@@ -26,8 +29,7 @@ public class SocketResourceTest {
 
     @Test
     public void onSocket() {
-
-
+        Assertions.assertThat("").isNotEmpty();
     }
 
 }
