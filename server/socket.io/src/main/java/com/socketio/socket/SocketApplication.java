0c6af685e90d6e77ecf21b4b5c49b5a5ea0b2fce
VitorManoelLP <vitor.m.lima.p@gmail.com>
2023-01-22 04:55:20 -0300

    ajustes socketIO

--- server/socket.io/src/main/java/com/socketio/socket/SocketApplication.java
+++ server/socket.io/src/main/java/com/socketio/socket/SocketApplication.java
@@ -2,12 +2,14 @@ package com.socketio.socket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SocketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SocketApplication.class, args);
+    }
 
 }
